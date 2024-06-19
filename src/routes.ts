e8eee2e2d65aec8a263cb19f13f8d717fed76a5b
jrpbjr <jrpbjr@gmail.com>
2024-06-19 12:57:36 -0300

    arquivo inicial de rotas

---
+++ src/routes.ts
@@ -0,0 +1,8 @@
+import { Router, Request, Response, request} from "express";
+
+const router = Router();
+router.get("/test"), (request: Request, response: Response) => {
+    return response.json({ok: true});
+}
+
+export { router };
