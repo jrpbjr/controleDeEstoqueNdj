4195c678f31d08f596d406064c55bc4349e12493
jrpbjr <jrpbjr@gmail.com>
2024-06-19 17:51:48 -0300

    criacao da service de usuario

--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,3 +1,45 @@
 import prismaClient from "../../prisma";
+import { hash } from "bcryptjs";
+import {UserRequest} from "../../models/interface/user/UserRequest"
 
-prismaClient
+class CreateService{
+
+    async execute({name,email,password}: UserRequest) {
+        if(!email){
+            throw new Error("Email incorrect");
+        }
+
+        const userAlreadyExist = await prismaClient.user.findFirst({
+            where: {
+                email: email
+            }
+        })
+
+        if (userAlreadyExist){
+            throw new Error("Email already exist");
+        }
+
+        //encriptando senha do usuário
+        const passwordHash = await hash(password, 8);
+
+        // Criando  nosso usuário
+
+        const user = prismaClient.user.create({
+            data: {
+                name: name,
+                email: email,
+                password: passwordHash
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true
+            }
+        })
+
+        return user;
+
+    }
+
+}
+export { CreateService }
