ca674e4230c91b8a1619525aa6746fcccdc40266
jrpbjr <jrpbjr@gmail.com>
2024-06-20 22:40:15 -0300

    Criando rota da autenticar usuario

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response} from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
+import { AuthUserController } from "./controllers/user/AuthUserController"
 
 const router = Router();
 router.get("/test", (request: Request, response: Response) => {
@@ -8,6 +9,7 @@ router.get("/test", (request: Request, response: Response) => {
 
 //User Routes
 router.post('/user',new CreateUserController().handle);
+router.post('/session', new AuthUserController().handle)
 
 
 export { router };
