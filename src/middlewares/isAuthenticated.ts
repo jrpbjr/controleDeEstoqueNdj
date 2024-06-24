081faf36908892c549429c06e3ad4e5b90db4e7b
jrpbjr <jrpbjr@gmail.com>
2024-06-23 21:14:50 -0300

    Ajuste na verificação do token

--- src/middlewares/isAuthenticated.ts
+++ src/middlewares/isAuthenticated.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { verify } from "jsonwebtoken";
-import { Payload } from "../models/interfaces/user/auth/Payload";
+import { Payload } from "../models/interface/user/auth/Payload";
+
 
 export function isAuthenticated(
     request: Request,
@@ -18,12 +19,16 @@ export function isAuthenticated(
     const [, token] = authToken.split(" ");
 
     try {
-
         // Validar token
-        const { sub } = verify(token, process.env.JWT_SECRET) as Payload;
+        const secret = process.env.JWT_SECRET;
+        if (!secret) {
+            throw new Error("JWT secret is not defined");
+        }
+
+        const { sub } = verify(token, secret) as Payload;
         request.user_id = sub;
         return next(); // Deixa que a requisição prossiga
     } catch (error) {
-        return response.send(401).end();
+        return response.status(401).end();
     }
 }
