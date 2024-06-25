429aacef8109800d45e0cf90c8bebaba898d6eb9
jrpbjr <jrpbjr@gmail.com>
2024-06-24 22:25:32 -0300

    criacao do controller categoria

---
+++ src/controllers/category/CreateCategoryController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { CategoryRequest } from "../../models/interface/user/category/CategoryRequest";
+import { CreateCategoryService } from "../../services/category/CreateCategoryService";
+
+class CreateCategoryController{
+
+    async handle(request: Request, response: Response){
+        const { name } : CategoryRequest = request.body;
+        const createCategoryService = new CreateCategoryService();    
+        const category = await createCategoryService.execute({name});
+        return response.json(category);
+    }
+
+
+}
+
+export {CreateCategoryController}
