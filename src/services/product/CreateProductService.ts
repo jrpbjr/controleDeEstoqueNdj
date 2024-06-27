53029f35c6cad15470de122781b6513baaf58f33
jrpbjr <jrpbjr@gmail.com>
2024-06-27 19:14:11 -0300

    Criando o servico de insercao de produto

---
+++ src/services/product/CreateProductService.ts
@@ -0,0 +1,23 @@
+import prismaClient from "../../prisma";
+import { ProductRequest } from "../../models/interface/product/Productrequest";
+
+class CreateProductService {
+
+    async execute({name, price, description, banner, category_id, amount}: ProductRequest){
+
+        const product = await prismaClient.product.create({
+            data: {
+                name: name,
+                price: price,
+                description: description,
+                banner: banner,
+                category_id: category_id,
+                amount: Number(amount)
+            }
+        });
+        return product;
+    }
+
+}
+
+export {CreateProductService}
