643de2920defb4805af02b78e6bd47ff5572efc3
jrpbjr <jrpbjr@gmail.com>
2024-07-10 22:18:34 -0300

    configurando swagger

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
+import swaggerUi from "swagger-ui-express";
+import path from "path";
+import swaggerDocument from "../swagger.json";
+import cors from "cors";
 import { router } from './routes';
 
 const app = express();
 const port = 3333;
 app.use(express.json());
-app.use(router);
+app.use(cors());
+app.use('v1',router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/files', express.static(path.resolve(__dirname, "..", "tmp")));
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
     if (err instanceof Error) {
@@ -18,6 +25,12 @@ app.use((err: Error, request: Request, response: Response, next: NextFunction) =
         message: 'Internal server error.'
     })
 })
+
+app.get('/terms', (request: Request, response: Response) =>{
+    return response.json({
+        message: "termos de serviço"
+    })
+});
 
 app.listen(port, () => {
     console.log("Servidor rodando na porta 3333 - Projeto Controle de Estoque curso NodeJs")
